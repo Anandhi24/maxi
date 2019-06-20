4290064a6b3b9c6a5010c25babf29be452d7de8b
Anandhi24 <46163458+Anandhi24@users.noreply.github.com>
2019-06-20 12:26:41 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,9 +1,10 @@
-s="abcda"
+s=input()
 list=[]
 list1=[]
 for i in range(len(s)):
-    if s[i] not in list:
+    if s[i] not in list and s[i].islower():
         list.append(s[i])
     else:
         list1.append(s[i])
 print(len(list))
+
